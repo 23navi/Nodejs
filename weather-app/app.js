be428c4d0f58d50fb58ccf395003012b61f55db2
Navi Sureka <navisureka23@gmail.com>
2021-05-31 14:39:42 +0530

    changes

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,25 @@
-const forecast = require("./weather.js");
-const geocode = require("./geocode.js");
+const forecast = require("./utils/weather.js");
+const geocode = require("./utils/geocode.js");
 
 
-place ="kolkata"
-geocode(place,(error,response)=>{
-    if(error){
-        console.log(error)
-    }else{
-        forecast(response.lat,response.long,(error,res)=>{
-            if(error){
-                console.log(error)
-            }else{
-                console.log(res)
-            }
-        })
-    }
-})
+place =process.argv[2]
+if(!place){
+    console.log("Please provide the location")
+}else{
+
+
+    geocode(place,(error,response)=>{
+        if(error){
+            console.log(error)
+        }else{
+            forecast(response.lat,response.long,(error,res)=>{
+                if(error){
+                    console.log(error)
+                }else{
+                    console.log(response.name)
+                    console.log(res)
+                }
+            })
+        }
+    })
+}
