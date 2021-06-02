0363f0653be11b2db8029cd80697d63058939593
Navi Sureka <navisureka23@gmail.com>
2021-06-02 20:47:45 +0530

    task

---
+++ task-manager/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task= require("./models/tasks")
+
+const app = express()
+const port = process.env.PORT || 8080
+
+
+app.use(express.json())
+
+app.get("",(req,res)=>{
+    res.send("heyy")
+})
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
