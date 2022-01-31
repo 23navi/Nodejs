14a0665fadfd3cc6b78ecb61d4045e9f232c25ba
Navi sureka <navisureka23@gmail.com>
2022-01-31 14:16:43 +0530

    task and user endpoing

--- task-app_node/src/index.js
+++ task-app_node/src/index.js
@@ -6,15 +6,41 @@ const Task=require("./models/task");
 const PORT= process.env.PORT || 3000;
 
 const app=express();
-app.use(express.json);
+app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("hello");
 })
 
+
+
+//New User post route
 app.post("/users",(req,res)=>{
-    res.send("user to be created");
+    console.log(req.body)
+    const newUser= new User(req.body);
+
+    newUser.save().then((user)=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.send(e);
+    })
+    
 })
+
+
+//New Task post route
+
+app.post("/tasks",(req,res)=>{
+    const newTask= new Task(req.body);
+    newTask.save().then(()=>{
+        res.status(201);
+        res.send(newTask);
+    }).catch(e=>{
+        res.status(400);
+        res.send(e);
+    })
+})
+
 
 app.listen(PORT,()=>{
     console.log("App up and running on port ",PORT);
