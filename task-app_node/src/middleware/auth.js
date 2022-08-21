e3ad667316bfb936f063c34d542c444b718c6bf4
Navi sureka <navisureka23@gmail.com>
2022-08-21 19:38:44 +0530

    new

--- task-app_node/src/middleware/auth.js
+++ task-app_node/src/middleware/auth.js
@@ -5,7 +5,7 @@ const User=require("../models/user")
 const auth=async (req,res,next)=>{
     try{
         const token=(req.header("Authorization").replace("Bearer ",""));
-        const decoded= jwt.verify(token,"NaviSureka");
+        const decoded= jwt.verify(token,process.env.JWT_SECRET);
         const user=await  User.findOne({"id":decoded.id,"tokens.token":token});   // "tokens.token" is special syntex for geting match from arr of obj in mongoose only
         if(!user) {
             console.log("Invalid");
